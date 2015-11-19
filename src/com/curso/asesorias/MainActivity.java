a4a09e58fef563a45ce8c109d27377042ebdd132
karlagutz <karlagutz07@gmail.com>
2015-11-18 17:47:06 -0700

    asdff

--- src/com/curso/asesorias/MainActivity.java
+++ src/com/curso/asesorias/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity  implements OnClickListener {
 
 
 	@Override
-	public void onClick(View arg0) {
+	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
 	}
